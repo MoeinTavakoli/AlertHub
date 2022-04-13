daa90a10c578b41e44a6d6654dc083c5577d651c
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-13 10:35:03 +0430

    error handling in connection database

--- db/_connection.js
+++ db/_connection.js
@@ -6,8 +6,8 @@ const config = require('../config');
 const client = new Client(config.databse);
 
 
-client.connect().catch(() => {
-  throw new Error('unable to connect to database !!!');
+client.connect().catch((err) => {
+  throw new Error('unable to connect to database !!!' , err);
 });
 
 module.exports = client;
