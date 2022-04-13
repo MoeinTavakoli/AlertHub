d211d5412e5f87bfdf5989daaa7441bce848cd37
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-13 10:50:21 +0430

    improve sending error in authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ const { verifyToken } = require('../utils/jwt');
 async function authentication(req, res, next) {
   try {
     const token = req.headers.authorization;
-    if (!token) return res.status(401).send('unathorized');
+    if (!token) return res.status(401).send('token not found !');
 
     const payload = verifyToken(token);
     if (!payload) {
