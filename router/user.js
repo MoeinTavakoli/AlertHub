387d5388746ceab6e0f260c856ceb38dc58733d0
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-16 14:56:18 +0430

    change route name updatePhone

--- router/user.js
+++ router/user.js
@@ -17,7 +17,7 @@ const controller = require('../controller/user');
 // routes
 app.post('/signup', controller.signup);
 app.post('/login', controller.login);
-app.put('/update', auth, controller.changePhoneNumber);
+app.put('/phone', auth, controller.changePhoneNumber);
 // 
 
 module.exports = app;
