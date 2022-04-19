46400a0e69de20fb74e0d4a9996c16fbb1f02543
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-19 16:49:48 +0430

    add function to remove url and link character (for kavenegar limitation to send link)

--- utils/removeURL.js
+++ utils/removeURL.js
@@ -4,9 +4,9 @@
  * @returns 
  */
 function removeUrlCharachter(url) {
-    return url.replace('http://', '').replace('https://', '').replace('www.', '').replace('.com', '').replace('.ir', '');
+  return url.replace('http://', '').replace('https://', '').replace('www.', '').replace('.com', '').replace('.ir', '');
 }
 
 
-module.exports = removeUrlCharachter
+module.exports = removeUrlCharachter;
 
