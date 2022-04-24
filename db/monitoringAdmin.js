a28cdd60afc83c7aad0007002d451396b6bbf6c2
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 08:34:48 +0430

    add function createMonitoringAdmin

---
+++ db/monitoringAdmin.js
@@ -0,0 +1,30 @@
+const prisma = require('../loader/prisma');
+
+/**
+ * 
+ * @param {String} username 
+ * @param {String} password 
+ * @param {String} phoneNumber 
+ * @returns 
+ */
+async function createMonitoringAdmin(username, password, phoneNumber) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    return await prisma.users.create({
+      data: {
+        username,
+        password,
+        phoneNumber,
+        role: 'MONITORING_ADMIN',
+      }
+    });
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+
+module.exports = {
+  createMonitoringAdmin
+};
