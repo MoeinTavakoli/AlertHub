80e6f1dadbba1af45d6ab9ed10e30f8d1b22223e
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 09:12:44 +0430

    add controller to deleteMonitoringAdmin

--- controller/monitoringAdmin.js
+++ controller/monitoringAdmin.js
@@ -12,12 +12,34 @@ async function createMonitoringAdmin(req, res) {
     const result = await db.createMonitoringAdmin(username, password, phoneNumber);
     if (!result) return res.status(400).send('create monitoring admin failed');
     res.send('monitoring admin created ...');
-  } catch (error) {
+  }
+  catch (error) {
+    res.status(400).send(error);
+  }
+}
+
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ */
+async function deleteMonitoringAdmin(req, res) {
+  try {
+    const username = req.params.username;
+    const result = await db.deleteMonitoringAdmin(username);
+    if (result.count == 0) return res.status(400).send('user didnt deleted  !!!');
+    res.send('monitorng admin deleted');
+  }
+  catch (error) {
     res.status(400).send(error);
   }
 }
 
 
+
+
+
 module.exports = {
-  createMonitoringAdmin
+  createMonitoringAdmin,
+  deleteMonitoringAdmin
 };
