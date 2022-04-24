f77c90b85f232b1d4f9e1ea5d23b4514c7103919
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 17:12:35 +0430

    router login monitorAdmin

--- router/monitoringAdmin.js
+++ router/monitoringAdmin.js
@@ -15,7 +15,7 @@ const controller = require('../controller/monitoringAdmin');
 
 
 
-
+app.post('/login', controller.login);
 app.post('/user/create', schemaValidator.createUser, auth, isMonitorAdmin, controller.createMonitoringAdmin);
 app.delete('/user/delete/:username', auth, isMonitorAdmin, controller.deleteMonitoringAdmin);
 app.put('/user/update/username/:username', schemaValidator.changeUsername, auth, isMonitorAdmin, controller.changeUsername);
