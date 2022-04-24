a3cd49930d9e9c7ee638bc809185c23d52c4e567
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 11:11:40 +0430

    add router updatePassword

--- router/monitoringAdmin.js
+++ router/monitoringAdmin.js
@@ -18,6 +18,7 @@ app.post('/user/create', controller.createMonitoringAdmin);
 app.delete('/user/delete/:username', controller.deleteMonitoringAdmin);
 app.put('/user/update/username/:username', controller.changeUsername);
 app.put('/user/update/password/:username', controller.changePassword);
+app.put('/user/update/phone/:username', controller.updatePhoneNumber);
 
 
 module.exports = app;
