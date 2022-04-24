f0e046ac7b975b63c7e6d44bbd5274d70ef310f6
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 08:50:35 +0430

    add error handling for createMonitoringAdmin into controller

--- controller/monitoringAdmin.js
+++ controller/monitoringAdmin.js
@@ -7,10 +7,14 @@ const db = require('../db/monitoringAdmin');
  * @param {import('express').Response} res 
  */
 async function createMonitoringAdmin(req, res) {
-  const { username, password, phoneNumber } = req.body;
-  const result = await db.createMonitoringAdmin(username, password, phoneNumber);
-  if (!result) return res.status(400).send('create monitoring admin failed');
-  res.send('monitoring admin created ...');
+  try {
+    const { username, password, phoneNumber } = req.body;
+    const result = await db.createMonitoringAdmin(username, password, phoneNumber);
+    if (!result) return res.status(400).send('create monitoring admin failed');
+    res.send('monitoring admin created ...');
+  } catch (error) {
+    res.status(400).send(error);
+  }
 }
 
 
