e72cbdd88551c86edb2d950f96c846f8deccecbc
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 12:51:12 +0430

    schema validator for deleteUser in monitorAdmin

--- router/monitoringAdmin.js
+++ router/monitoringAdmin.js
@@ -15,7 +15,7 @@ const controller = require('../controller/monitoringAdmin');
 
 
 app.post('/user/create', validator.createUser, controller.createMonitoringAdmin);
-app.delete('/user/delete/:username', controller.deleteMonitoringAdmin);
+app.delete('/user/delete/:username',validator.deleteUser , controller.deleteMonitoringAdmin);
 app.put('/user/update/username/:username', controller.changeUsername);
 app.put('/user/update/password/:username', controller.changePassword);
 app.put('/user/update/phone/:username', controller.updatePhoneNumber);
