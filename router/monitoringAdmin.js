9c301d27ebda6a55b1b733104310e29df29e1fe4
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-24 13:00:43 +0430

    add schema validator in changeUsername monitorAdmin

--- router/monitoringAdmin.js
+++ router/monitoringAdmin.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 // middleware
-const validator = require('../middleware/validator/monitorAdmin');
+const schemaValidator = require('../middleware/validator/monitorAdmin');
 // 
 
 
@@ -14,9 +14,9 @@ const controller = require('../controller/monitoringAdmin');
 
 
 
-app.post('/user/create', validator.createUser, controller.createMonitoringAdmin);
-app.delete('/user/delete/:username',validator.deleteUser , controller.deleteMonitoringAdmin);
-app.put('/user/update/username/:username', controller.changeUsername);
+app.post('/user/create', schemaValidator.createUser, controller.createMonitoringAdmin);
+app.delete('/user/delete/:username', schemaValidator.deleteUser, controller.deleteMonitoringAdmin);
+app.put('/user/update/username/:username', schemaValidator.changeUsername, controller.changeUsername);
 app.put('/user/update/password/:username', controller.changePassword);
 app.put('/user/update/phone/:username', controller.updatePhoneNumber);
 
