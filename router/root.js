20102b6826b9211780c99498c4ce895859f8f632
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-26 09:44:54 +0430

    add schemaValdator root into router

--- router/root.js
+++ router/root.js
@@ -4,13 +4,14 @@ const app = express();
 // middleware
 const auth = require('../middleware/auth');
 const isRoot = require('../middleware/isRoot');
+const schemaValdator = require('../middleware/validator/root');
 // 
 
 // controller
 const controller = require('../controller/root');
 // 
 
-app.post('/login', controller.loginRoot);
+app.post('/login',schemaValdator.login, controller.loginRoot);
 app.post('/admin-monia/create', auth, isRoot, controller.createMoniaAdmin);
 app.delete('/admin-monia/delete', auth, isRoot, controller.removeMoniaAdmin);
 
