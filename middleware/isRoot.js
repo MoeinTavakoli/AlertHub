cd76376feb0fe1e4c9af9728f012bcb3712c8446
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-26 17:35:44 +0430

    bug fix at send two response

--- middleware/isRoot.js
+++ middleware/isRoot.js
@@ -4,9 +4,9 @@
  * @param {*} res 
  * @param {*} next 
  */
-function isRoot(req,res,next) {
-  if (req.info.role !== 'ROOT') {
-    res.status(401).send('permission denied !');
+function isRoot(req, res, next) {
+  if (req.info.role !== 'ROOT' ) {
+    return res.status(401).send('permission denied !');
   }
   next();
 }
