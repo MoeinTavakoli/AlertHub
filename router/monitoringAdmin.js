201c603cad77deafb4b722c28e9f5c3184e76b12
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-04-27 12:43:23 +0430

    change route name

--- router/monitoringAdmin.js
+++ router/monitoringAdmin.js
@@ -17,7 +17,7 @@ const controller = require('../controller/monitoringAdmin');
 
 
 app.post('/login', schemaValidator.login, controller.login);
-app.post('/create', schemaValidator.createUser, auth, isMonitorAdmin, checkRole, controller.createMonitoringAdmin);
+app.post('/user/create', schemaValidator.createUser, auth, isMonitorAdmin, checkRole, controller.createMonitoringAdmin);
 app.delete('/delete/:username', auth, isMonitorAdmin, checkRole, controller.deleteMonitoringAdmin);
 app.put('/username/:username', schemaValidator.changeUsername, auth, isMonitorAdmin, checkRole, controller.changeUsername);
 app.put('/password/:username', schemaValidator.changePassword, auth, isMonitorAdmin, checkRole, controller.changePassword);
--- router/root.js
+++ router/root.js
@@ -13,8 +13,8 @@ const controller = require('../controller/root');
 // 
 
 app.post('/login', schemaValdator.login, controller.loginRoot);
-app.post('/admin-monia/create', schemaValdator.createMoniaAdmin, auth, isRoot, checkRole, controller.createMoniaAdmin);
-app.delete('/admin-monia/delete', schemaValdator.removeMoniaAdmin, auth, isRoot, checkRole, controller.removeMoniaAdmin);
+app.post('/user/create', schemaValdator.createMoniaAdmin, auth, isRoot, checkRole, controller.createMoniaAdmin);
+app.delete('/user/delete', schemaValdator.removeMoniaAdmin, auth, isRoot, checkRole, controller.removeMoniaAdmin);
 
 
 module.exports = app;
