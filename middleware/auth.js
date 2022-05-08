91c320ce255f6cbc017700bb5226b8d05621cb26
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-05-08 10:43:16 +0430

    add auth payload token and database

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const { verifyToken } = require('../utils/jwt');
+const prisma = require('../loader/prisma');
 
 /**
  * 
@@ -14,13 +15,42 @@ async function authentication(req, res, next) {
     if (!payload) {
       return res.status(401).send('unathorized');
     }
+
+    if (await checkRole(payload.username , payload.role) == 0 ) {
+      return res.status(401).send('token is not valid');
+    }
+
     req.info = payload;
     next();
   }
-  catch (err){
+  catch (err) {
     return res.status(401).send('unathorized');
   }
 }
+
+
+
+/**
+ * 
+ * @param {String} username 
+ * @param {String} role 
+ */
+async function checkRole(username, role) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    return await prisma.users.count({
+      where: {
+        username,
+        role,
+        isDeleted: false
+      }
+    });
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
 
 
 module.exports = authentication;
