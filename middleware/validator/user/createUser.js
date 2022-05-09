f6160b7dad95133b6e9472b7931981a1afb5aa3f
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-05-09 09:59:42 +0430

    add valdation for phone number

--- middleware/validator/user/createUser.js
+++ middleware/validator/user/createUser.js
@@ -8,7 +8,10 @@ function createUserValidator(req, res, next) {
   if (!req.body.username ||
     !req.body.password ||
     !req.body.phoneNumber ||
-    !(/^-?\d+$/.test(req.body.phoneNumber))) return res.status(400).send('please insert username , password , valid phoneNumber in body ');
+    !(/^-?\d+$/.test(req.body.phoneNumber)) 
+    ||
+    req.body.phoneNumber.length !== 11
+  ) return res.status(400).send('please insert username , password , valid phoneNumber in body ');
   if (req.body.role && !roles.includes(req.body.role)) return res.status(400).send('invalid role !!');
   if (req.body.password.trim().length < 5) return res.status(400).send('please insert password at least 5 character');
 
