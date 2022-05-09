fc87f78b980f22ceac5e7ef6e06e379e274ab806
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-05-09 09:46:24 +0430

    condition length for password in middleware

--- middleware/validator/user/changePassword.js
+++ middleware/validator/user/changePassword.js
@@ -6,6 +6,8 @@
 function changePasswordValditor(req, res, next) {
 
   if (!req.body.password) return res.status(400).send('please insert password in body');
+  if (req.body.password.trim().length < 5) return res.status(400).send('please insert password at least 5 character');
+
   next();
 
 }
--- middleware/validator/user/createUser.js
+++ middleware/validator/user/createUser.js
@@ -10,6 +10,8 @@ function createUserValidator(req, res, next) {
     !req.body.phoneNumber ||
     !(/^-?\d+$/.test(req.body.phoneNumber))) return res.status(400).send('please insert username , password , valid phoneNumber in body ');
   if (req.body.role && !roles.includes(req.body.role)) return res.status(400).send('invalid role !!');
+  if (req.body.password.trim().length < 5) return res.status(400).send('please insert password at least 5 character');
+
   next();
 }
 
