b434388418f74df1e6cd26bdcbb0775137634d09
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-05-14 16:48:44 +0430

    update paramas routes

--- router/user.js
+++ router/user.js
@@ -17,7 +17,7 @@ app.get('/info' ,auth ,defineAccessRoles,  controller.getAllUsers);
 app.post('/login', schemaValidator.login, controller.login);
 app.post('/create', schemaValidator.createUser, auth, checkPermission, controller.createUser);
 app.delete('/delete/:username', auth, checkPermission, controller.deleteUser);
-app.put('/phone/:username', schemaValidator.updatePhone, auth, checkPermission, controller.updatePhoneNumber);
+app.put('/phone/:userID', schemaValidator.updatePhone, auth, checkPermission, controller.updatePhoneNumber);
 app.put('/password/:userID', schemaValidator.updatePassword, auth, checkPermission, controller.updatePassword);
 app.put('/username/:userID', schemaValidator.updateUsername, auth, checkPermission, controller.updateUsername);
 
