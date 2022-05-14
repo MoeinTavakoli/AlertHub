6f8ba34b2431d191e1210401e0ea64a0f3e21ca1
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-05-14 13:20:47 +0430

    change req.params username --> userID

--- router/user.js
+++ router/user.js
@@ -18,8 +18,8 @@ app.post('/login', schemaValidator.login, controller.login);
 app.post('/create', schemaValidator.createUser, auth, checkPermission, controller.createUser);
 app.delete('/delete/:username', auth, checkPermission, controller.deleteUser);
 app.put('/phone/:username', schemaValidator.updatePhone, auth, checkPermission, controller.updatePhoneNumber);
-app.put('/password/:username', schemaValidator.updatePassword, auth, checkPermission, controller.updatePassword);
-app.put('/username/:username', schemaValidator.updateUsername, auth, checkPermission, controller.updateUsername);
+app.put('/password/:userID', schemaValidator.updatePassword, auth, checkPermission, controller.updatePassword);
+app.put('/username/:userID', schemaValidator.updateUsername, auth, checkPermission, controller.updateUsername);
 
 
 module.exports = app;
