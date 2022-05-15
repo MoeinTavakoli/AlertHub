e9ccc4c518e33ba3519342eca0e67d4c3fbe1d3c
moeen.tavakoli <mtawakoli2283@gmail.com>
2022-05-15 14:57:54 +0430

    update paramas router deleteUser

--- router/user.js
+++ router/user.js
@@ -16,7 +16,7 @@ const controller = require('../controller/user');
 app.get('/info' ,auth ,defineAccessRoles,  controller.getAllUsers);
 app.post('/login', schemaValidator.login, controller.login);
 app.post('/create', schemaValidator.createUser, auth, checkPermission, controller.createUser);
-app.delete('/delete/:username', auth, checkPermission, controller.deleteUser);
+app.delete('/delete/:userID', auth, checkPermission, controller.deleteUser);
 app.put('/phone/:userID', schemaValidator.updatePhone, auth, checkPermission, controller.updatePhoneNumber);
 app.put('/password/:userID', schemaValidator.updatePassword, auth, checkPermission, controller.updatePassword);
 app.put('/username/:userID', schemaValidator.updateUsername, auth, checkPermission, controller.updateUsername);
